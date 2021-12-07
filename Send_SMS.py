703a500b2432e8beaef92a3113ba4552fe221cb2
SB Components <gajender.singh@sb-components.co.uk>
2021-12-07 16:57:36 +0530

    Update Send_SMS.py

--- Send_SMS.py
+++ Send_SMS.py
@@ -4,7 +4,8 @@ import machine
 import os
 import utime
 import binascii
-
+import time
+import Lcd1_14driver
 
 led_pin = 25  # Pico onboard Led
 pwr_enable = 22  # SIM868 Pwrkey connected on GP22
@@ -17,6 +18,7 @@ print(os.uname())
 
 # Initialize on board led as output
 led_onboard = machine.Pin(led_pin, machine.Pin.OUT)
+LCD = Lcd1_14driver.Lcd1_14()#driver of lcd display
 
 def hexstr_to_str(hex_str):
     hex_data = hex_str.encode('utf-8')
@@ -38,6 +40,33 @@ def blink_led():
     utime.sleep(1)
     led_onboard(0)
 
+def lcd_border():
+        LCD.hline(10,10,220,LCD.white)
+        LCD.hline(10,125,220,LCD.white)
+        LCD.vline(10,10,115,LCD.white)
+        LCD.vline(230,10,115,LCD.white)       
+        LCD.lcd_show()
+    
+def infoDevice():
+        LCD.fill(LCD.black) 
+        LCD.lcd_show()
+        lcd_border()
+        
+        LCD.text("SB-COMPONENTS",70,40,LCD.white)
+        LCD.text("PICO 2G",70,60,LCD.white)
+        LCD.text("EXPANSION",70,80,LCD.white)  
+        LCD.lcd_show()
+        time.sleep(2)
+        LCD.fill(LCD.black)
+        lcd_border()
+        LCD.text("WAITING.....",70,40,LCD.white)
+        LCD.lcd_show()
+        x = 0
+        for y in range(0,1):
+             x += 4
+             LCD.text("......",125+x,40,LCD.white)
+             LCD.lcd_show()
+             time.sleep(1)
 
 # power on/off the module
 def power_on_off():
@@ -105,11 +134,25 @@ def check_start():
         rec_temp = wait_resp_info()
         if 'OK' in rec_temp.decode():
             print('Pico 2G is ready\r\n' + rec_temp.decode())
+            LCD.fill(LCD.black) 
+            LCD.lcd_show()
+            lcd_border()
+
+            LCD.text("Pico 2G is ready",40,40,LCD.white) 
+            LCD.lcd_show()
             break
         else:
             power_on_off()
             print('Pico 2G is starting up, please wait...\r\n')
+            LCD.fill(LCD.black) 
+            LCD.lcd_show()
+            lcd_border()
+
+            LCD.text("Pico 2G is starting up",40,40,LCD.white)
+            LCD.text("Please wait...",40,60,LCD.white) 
+            LCD.lcd_show()
             utime.sleep(8)
+
 
 
 # Check the network status
@@ -117,9 +160,21 @@ def network_check():
     for i in range(1, 3):
         if send_cmd("AT+CGREG?", "0,1") == 1:
             print('Pico 2G is online\r\n')
+            LCD.fill(LCD.black) 
+            LCD.lcd_show()
+            lcd_border()
+
+            LCD.text("Pico 2G is online",40,40,LCD.white) 
+            LCD.lcd_show()
             break
         else:
             print('Pico 2G is offline, please wait...\r\n')
+            LCD.fill(LCD.black) 
+            LCD.lcd_show()
+            lcd_border()
+
+            LCD.text("Pico 2G is offline",40,40,LCD.white) 
+            LCD.lcd_show()
             utime.sleep(5)
             continue
 
@@ -135,6 +190,7 @@ def send_sms(phone_num='12345', sms_text=""):
 
 
 # main function call
+infoDevice()
 blink_led()  # Test Led
 check_start() # Initialize SIM Module 
 network_check() # Network connectivity check
@@ -142,3 +198,4 @@ network_check() # Network connectivity check
 # SMS test
 sms_text ="Hello from Pico 2G."
 send_sms("123456", sms_text)
+
